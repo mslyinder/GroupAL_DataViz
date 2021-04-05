33db61aabd540b4c1391c08121e918c88b2ec5a7
mslyinder <74016626+mslyinder@users.noreply.github.com>
2021-04-05 22:38:38 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,14 +6,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 from dash.dependencies import Input, Output
-import numpy as np
 import dash_daq as daq
-from urllib.request import urlopen
 ############################################################################################################################################################
-
-import json
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
 
 
 
@@ -31,12 +25,13 @@ colors = {
 
 '''LOADING DATA'''
 st = pd.read_csv('weekly_deaths.csv' , sep = ',').fillna(0)
-df = pd.read_csv('USA_DISEASES.csv').drop(columns = ['Unnamed: 0', 'MMWR Year','MMWR Week', 'Jurisdiction of Occurrence'])
+df = pd.read_csv('total_selected_diseases.csv').drop(columns = ['Unnamed: 0', 'Jurisdiction of Occurrence', 'MMWR Year', 'MMWR Week'])
 dataset = pd.read_csv('selected_diseases.csv')
-##############################################################################
-diseases1 = df.iloc[:, 1:-1].columns
 
-diseases = st.iloc[:, 6:-1].columns
+##############################################################################
+diseases1 = df.iloc[:, 1:].columns
+
+diseases = st.iloc[:, 6:].columns
 
 
 st['Week Ending Date'] = pd.to_datetime(st['Week Ending Date'])
@@ -71,9 +66,9 @@ tab_style = {
     'box-shadow': '2px' '2px' '2px' '#cf9696',
     'backgroundColor': '#dce6ef',
     'border-radius': '7px',
-    'height': '70px',
-    'height': '130%',
-    'font-size': '25px',
+    'height': '80px',
+    'height': '150%',
+    'font-size': '19px',
     'width': '100%',
     'font-family':"Calibri (Body)"
     
@@ -86,8 +81,8 @@ tab_selected_style = {
     'color': 'black',
     'padding': '10px',
     'border-radius': '7px',
-    'height': '70px',
-    'font-size': '25px',
+    'height': '80px',
+    'font-size': '19px',
     'fontWeight': 'bold',
     'width': '100%',
     'height': '130%',
@@ -102,10 +97,10 @@ app.layout = html.Div(children=[
         html.Div(children = [
     
        
-             html.P('Deaths by Selected Diseases in the USA', style = {'fontSize': 50, 'text-align': 'center', 'margin-bottom': 0,
+             html.P('Deaths by Diseases in the USA', style = {'fontSize': 35, 'text-align': 'center', 'margin-bottom': 0,
                                                                        'fontWeight': 'bold',
                                                                        'font-family':"Calibri (Body)"}),
-             html.P('Counts of Deaths by Select Causes 2014 to 2021', style = {'fontSize': 30, 
+             html.P('Counts of Deaths by Selected Causes from 2014 to 2021', style = {'fontSize': 25, 
                                                                                          'text-align': 'center' , 'margin-top': 0, 
                                                                                          'margin-bottom': 20,
                                                                                          'font-family':"Calibri (Body)"}),
@@ -114,18 +109,18 @@ app.layout = html.Div(children=[
         
     
     dcc.Tabs( style = {#'width': '80%', 'height': '80%', 
-                      'margin-right': '10%', 'margin-left': '10%'
+                      'margin-right': '17%', 'margin-left': '17%'
                       },
              id="tabs", value='tab-1', children=[
         dcc.Tab(label='Deaths Timeline', value='tab-1', style=tab_style, selected_style=tab_selected_style),
         dcc.Tab(label='Deaths by State', value='tab-2', style=tab_style, selected_style=tab_selected_style),
         dcc.Tab(label='Deaths Map', value='tab-4', style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Table', value='tab-5', style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Sources/Downloads', value='tab-6', style=tab_style, selected_style=tab_selected_style),
+        dcc.Tab(label='Data Table', value='tab-5', style=tab_style, selected_style=tab_selected_style),
+        dcc.Tab(label='More Info', value='tab-6', style=tab_style, selected_style=tab_selected_style),
     ]),
     
      html.Div(style = {#'width': '80%', 'height': '80%', 
-                      'margin-right': '10%', 'margin-left': '10%'
+                      'margin-right': '16%', 'margin-left': '16%'
                       },
                          id='tabs-content'
     ),
@@ -149,7 +144,7 @@ def render_content(tab):
     if tab == 'tab-1':
         return html.Div([
             html.P('Deaths Timeline of the Selected Diseases, from January 2014 to March 2021', 
-                   style = {'fontSize': 26, 
+                   style = {'fontSize': 25, 
                             'text-align': 'center' , 'margin-top': 0, 
                             'margin-bottom': 10,
                             'font-family':"Calibri (Body)"}),
@@ -176,22 +171,25 @@ def render_content(tab):
                 options = [dict(label = 'Yearly', value = 1), 
                            dict(label = 'Monthly', value = 0)],
                 value = 0,
-                style = {'margin-top': 20, 
+                style = {'margin-top': 65, 
                             'margin-bottom': 0,
-                         'margin-left': '2%'}
+                         'margin-left': '1.5%',
+                         'font-size': '18px'}
                 ),
             dcc.RadioItems(
                 id = 'aggregates',
                 options = [dict(label = 'Mean', value = 0), 
                            dict(label = 'Sum', value = 1)],
                 value = 0,
-                style = {
-                         'margin-left': '2%'}
+                style = {'font-size': '18px',
+                         'margin-top': 8, 
+                            'margin-bottom': 0,
+                         'margin-left': '1.8%'}
                 ),
             dcc.Graph(
                 id='lineplot',
                 figure=dict(layout=dict(autosize=True)),
-                      style={'margin-top': 20,
+                      style={'margin-top': 5,
                                 'height': '100%',
                                 'width': '100%'}
                 )],
@@ -202,12 +200,12 @@ def render_content(tab):
     
     elif tab == 'tab-2':
         return html.Div([
-            html.P('The Top Ten States with the Highest Number of Deaths by Disease', 
-                   style = {'fontSize': 26, 
+            html.P('Top 10 States with the Highest Number of Deaths by Disease', 
+                   style = {'fontSize': 30, 
                             'text-align': 'center' , 'margin-top': 0, 
                             'margin-bottom': 10,
                             'font-family':"Calibri (Body)"}),
-            html.P('The Total Number of Deaths Between Years or by Year', 
+            html.P('Total Number of Deaths Between Years or by Year', 
                    style = {'fontSize': 20, 
                             'text-align': 'center' , 'margin-top': 0, 
                             'margin-bottom': 10,
@@ -239,7 +237,7 @@ def render_content(tab):
                 marks={str(i): '{}'.format(str(i)) for i in
                        [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]},
                 className = 'box',
-                tooltip=dict(always_visible = True)
+                
                 ),
                 ],
             
@@ -252,7 +250,7 @@ def render_content(tab):
         return html.Div([
             
             html.P('Geographic Distribution of each Disease', 
-                   style = {'fontSize': 26, 
+                   style = {'fontSize': 30, 
                             'text-align': 'center' , 'margin-top': 0, 
                             'margin-bottom': 10,
                             'font-family':"Calibri (Body)"}),
@@ -290,7 +288,7 @@ def render_content(tab):
                 marks={str(i): '{}'.format(str(i)) for i in
                        [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]},
                 handleLabel={"showCurrentValue": True,"label": " "},
-                size=1300,
+                size=800,
                 color = 'cf9696',
                 className = 'box'
                 
@@ -311,13 +309,15 @@ def render_content(tab):
                 'minWidth': '180px', 'width': '180px', 'maxWidth': '180px',
                 'overflow': 'hidden',
                 'textOverflow': 'ellipsis',
-                'fontSize': '16px',
-                'padding': '5px'},
+                'fontSize': '10px',
+                'padding': '5px',
+                'font-family':"Calibri (Body)"},
                 style_header={
                     'backgroundColor': '#F0AB9E',
                     'fontWeight': 'bold', 
-                    'fontSize': '20px',
-                    'overflow': 'hidden'
+                    'fontSize': '15px',
+                    'overflow': 'hidden',
+                    'font-family':"Calibri (Body)"
                     },
                 style_cell_conditional=[
         {
@@ -333,6 +333,47 @@ def render_content(tab):
         return html.Div([
             
             
+                    html.P('Diseases Description:',
+                           style = {'fontSize': 26, 
+                            'text-align': 'left' , 
+                            'margin-top': 10, 
+                            'margin-bottom': 10,
+                            'font-family':"Calibri (Body)"}),
+                            
+                        
+                     dcc.Markdown('''
+                        * **All Cause:** deaths caused by all the diseases in this dataset
+                            
+                        * **Natural Causes**
+                            
+                        * **Septicemia:** (blood poisoning) invasion of the bloodstream by pathogenic agents and especially bacteria along with their toxins from a localized infection (as of the lungs or skin) that is accompanied by acute systemic illness
+                            
+                        * **Malignant Neoplasm:** a cancerous tumor, an abnormal growth that can grow uncontrolled and spread to other parts of the body
+                            
+                        * **Diabetes Mellitus:** (sugar diabetes) condition that occurs when the body can't use glucose (a type of sugar) normally
+                            
+                        * **Alzheimer:** progressive neurologic disorder that causes the brain to shrink (atrophy) and brain cells to die.
+                            
+                        * **Influenza & Pnemonia:** viral infection that attacks your respiratory system — your nose, throat and lungs
+                            
+                        * **Chronic lower Respiratory Disease:** a group of pathologies that obstruct the lungs, which mainly includes chronic obstructive pulmonary disease (COPD) and asthma. 
+                            
+                        * **Other Diseases of Respiratory System:** diseases that affect the air passages, including the nasal passages, the bronchi and the lungs.
+                            
+                        * **Nephritis, Nephrotic Syndrome and Nephrosis:** A condition in which the tissues in the kidney become inflamed and have problems filtering waste from the blood
+                            
+                        * **Symptoms, Signs and Abnormal Clinical and Laboratory Findings Not Elsewhere Found**
+                            
+                        * **Diseases of Heart:** a range of conditions that affect your heart
+                            
+                        * **Multiple Cause of Death + COVID-19**
+                            
+                        * **COVID-19 as Underlying Cause of Death**
+                            
+                         ''', style = {"margin-left": '30px',
+                         "margin-bottom": '30px'}),
+            
+            
            
                        
                     html.P('Data Sources:',
@@ -344,16 +385,16 @@ def render_content(tab):
                             
                    dcc.Markdown('''    
                                 
-                        Weekly Counts of Deaths by State and Select Causes
+                        **Weekly Counts of Deaths by State and Select Causes**
                             
-                        Data Time Span:         January 1, 2014 - March 13, 2021'
+                        **Data Time Span:**         January 1, 2014 - March 13, 2021
                             
-                        Data Published By:     National Center for Health Statistics
+                        **Data Published By:**     National Center for Health Statistics
                             
-                        For More Details Visit: ['https://data.cdc.gov/NCHS/Weekly-Counts-of-Deaths-by-State-and-Select-Causes/3yf8-kanr'] 
+                        **For More Details Visit:** ['https://data.cdc.gov/NCHS/Weekly-Counts-of-Deaths-by-State-and-Select-Causes/3yf8-kanr'] 
                         ''', style = {"margin-left": '30px'}),
                         
-                        html.P('Downloads:',
+                    html.P('Downloads:',
                            style = {'fontSize': 26, 
                             'text-align': 'left' , 
                             'margin-top': 10, 
@@ -362,9 +403,12 @@ def render_content(tab):
                             
                         
                      dcc.Markdown('''
-                        For More Details Visit: [https://data.cdc.gov/NCHS/Weekly-Counts-of-Deaths-by-State-and-Select-Causes/3yf8-kanr']
+                        **Download Data From the Source:** [https://data.cdc.gov/NCHS/Weekly-Counts-of-Deaths-by-State-and-Select-Causes/3yf8-kanr']
                          ''', style = {"margin-left": '30px'}),
-            
+                         
+                         
+                         
+                    
         
         
     ] , style={'textAlign': 'left',
@@ -384,15 +428,15 @@ def render_content(tab):
     
       
 def lineplot(disease, time, agg):
-    gg = pd.read_csv('USA_DISEASES.csv').drop(columns = ['Unnamed: 0','MMWR Week','MMWR Year','Jurisdiction of Occurrence'])
+    gg = pd.read_csv('total_selected_diseases.csv').drop(columns = ['Unnamed: 0', 'Jurisdiction of Occurrence', 'MMWR Year', 'MMWR Week'])
     gg['Week Ending Date'] = pd.to_datetime(gg['Week Ending Date'])
     gg = gg.set_index('Week Ending Date')
     df1 = gg.groupby(pd.Grouper(freq="M")).mean().reset_index()
     data_line = []
-    colory = ['rgb(0, 147, 146)','rgb(69, 176, 175)','rgb(114, 170, 161)','rgb(130, 181, 135)',
-                  'rgb(177, 199, 179)','rgb(207, 250, 211)', 'rgb(241, 234, 200)','rgb(250, 235, 162)',
-                      'rgb(255, 226, 87)','rgb(229, 185, 173)','rgb(250, 167, 145)','rgb(217, 137, 148)',
-                          'rgb(208, 88, 126)']
+    colory = ['rgb(0, 147, 146)','rgb(241, 234, 200)','rgb(250, 235, 162)',
+                      'rgb(255, 226, 87)','rgb(229, 185, 173)','rgb(69, 176, 175)','rgb(114, 170, 161)','rgb(130, 181, 135)',
+                  'rgb(177, 199, 179)','rgb(207, 250, 211)', 'rgb(250, 167, 145)','rgb(217, 137, 148)',
+                          'rgb(208, 88, 126)', 'rgb(250, 167, 145)','rgb(217, 137, 148)','rgb(250, 167, 145)','rgb(217, 137, 148)']
 
     for i in range(len(disease)):
         if time == 0:
@@ -418,7 +462,7 @@ def lineplot(disease, time, agg):
                            
                            autosize=False,
                             #width=1300,
-                            height=650,
+                            height=550,
                             margin=dict(
                                 l=50,
                                 r=50,
@@ -456,7 +500,7 @@ def top10(disease, date):
                            plot_bgcolor= 'rgba(0,0,0,0)',
                            font = dict(size = 15, color = 'black'),
                             #width=1200,
-                            height=650,
+                            height=550,
                             margin=dict(
                                 l=50,
                                 r=50,
@@ -470,6 +514,7 @@ def top10(disease, date):
     ))
     
     fig.layout.coloraxis.colorbar.title = ''
+    fig.update_yaxes(title_text = ' ')
     
     return fig
 
@@ -504,7 +549,7 @@ def display_choropleth(disease, year):
                             autosize=False,
                            # marker =  dict('colorbar' : dict('len': '0.7')),
                             #width=1200,
-                            height=650,
+                            height=550,
                             hoverlabel=dict(
                             bgcolor="white",
                             font_size=16,
